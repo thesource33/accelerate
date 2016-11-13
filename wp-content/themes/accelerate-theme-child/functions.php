73581246a6cc9206052aed01fbca985892af82d4
thesource33 <debshill3333@gmail.com>
2016-11-13 09:21:26 +0000

    acf added

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -24,20 +24,18 @@
  * @subpackage Accelerate Marketing
  * @since Accelerate Marketing 1.0
  */
-// Custom post types function
-function create_custom_post_types() {
-// create a case study custom post type
-    register_post_type( 'case_studies',
-        array(
-            'labels' => array(
-                'name' => __( 'Case Studies' ),
-                'singular_name' => __( 'Case Study' )
-            ),
-            'public' => true,
-            'has_archive' => true,
-            'rewrite' => array( 'slug' => 'case-studies' ),
-        )
-    );
-}
-// Hook this custom post type function into the theme
-add_action( 'init', 'create_custom_post_types' );
+
+ function create_custom_post_types() {
+     register_post_type( 'case_studies',
+         array(
+             'labels' => array(
+                 'name' => __( 'Case Studies' ),
+                 'singular_name' => __( 'Case Study' )
+             ),
+             'public' => true,
+             'has_archive' => true,
+             'rewrite' => array( 'slug' => 'case-studies' ),
+         )
+     );
+ }
+ add_action( 'init', 'create_custom_post_types' );
